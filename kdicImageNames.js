332d02fdfde37d71053a5fc15a7af91e30da4478
Addison Gould <gouldadd@grinnell.edu>
2017-08-26 15:45:42 -0500

    added download link for KDIC

--- kdicImageNames.js
+++ kdicImageNames.js
@@ -1,19 +1,19 @@
 var kdicImageNames = [
-  "ACDC.jpg",
+  "ACDC.jpg", 
   "Anti-Drug.jpg", 
   "Batman.jpg", 
   "BieberDrink.jpg", 
   "BlueTrain.jpg", 
-  "BrainOnKDIC.jpg",
+  "BrainOnKDIC.jpg", 
   "CamelCigs.jpg", 
-  "CaptainKDIC.jpg",
-  "CrystalSkull.jpg",
+  "CaptainKDIC.jpg", 
+  "CrystalSkull.jpg", 
   "ET.jpg", 
   "Flash.jpg", 
   "GetKidsHooked.jpg", 
   "GreenLantern.jpg", 
   "Heat.jpg", 
-  "HeCanFly.jpg",
+  "HeCanFly.jpg", 
   "HulkWolverine.jpg", 
   "Jaws.jpg", 
   "Junkie.jpg", 
@@ -21,17 +21,17 @@ var kdicImageNames = [
   "KimJong-il.jpg", 
   "McGruff.jpg", 
   "MoreMinerals.jpg", 
-  "NewYorker.jpg", 
+  "NewYorker.jpg",
   "NotoriousBIG.jpg", 
   "NukeEmFirst.jpg", 
   "Spongebob.jpg", 
   "StarWars.jpg", 
-  "TheChronic.jpg", 
+  "TheChronic.jpg",
   "ToBloodyKDIC.jpg", 
-  "TribeCalledQuest.jpg", 
+  "TribeCalledQuest.jpg",
   "WakeUp.jpg", 
-  "ZacEfron.jpg", 
-  "Overflow.jpg"
+  "ZacEfron.jpg",
+  "Overflow.jpg", 
 ];
 
 function numberOfImages(kdicImageNames) {
@@ -50,7 +50,7 @@ var iterCounter = Math.ceil(kdicImageNames.length / 3);
 
 $(document).ready(function(){
   for(var i = 0; i< iterCounter; i++) {
-    $("#testDiv").append('<tr><th> <a href="images/kdic_images/' + kdicImageNames[imageCounter] + '" data-lighter> <img src="images/kdic_images/' + kdicImageNames[imageCounter] +  '" alt="kdic image" class="kdic_image"> </a></th><th>  <a href="images/kdic_images/' + kdicImageNames[imageCounter + 1] + '" data-lighter> <img src="images/kdic_images/' + kdicImageNames[imageCounter + 1] + '" alt="kdic image" class="kdic_image"/> </a>  </th><th> <a href="images/kdic_images/' + kdicImageNames[imageCounter + 2] + '" data-lighter> <img src="images/kdic_images/' + kdicImageNames[imageCounter + 2] + '" alt="kdic image" class="kdic_image"> </a> </th> </tr>' );
+    $("#testDiv").append('<tr><th> <a href="images/kdic_images/' + kdicImageNames[imageCounter] + '" data-lighter> <img src="images/kdic_images/' + kdicImageNames[imageCounter] +  '" alt="kdic image" class="kdic_image"> </a> <a href="images/kdic_images/' + kdicImageNames[imageCounter] + '" download> <p>Download</p> </a></th><th>  <a href="images/kdic_images/' + kdicImageNames[imageCounter + 1]+ '" data-lighter> <img src="images/kdic_images/' + kdicImageNames[imageCounter + 1]+ '" alt="kdic image" class="kdic_image"/> </a> <a href="images/kdic_images/' + kdicImageNames[imageCounter + 1] + '" download> <p>Download</p> </a> </th><th> <a href="images/kdic_images/' + kdicImageNames[imageCounter + 2] + '" data-lighter> <img src="images/kdic_images/' + kdicImageNames[imageCounter + 2] + '" alt="kdic image" class="kdic_image"> </a> <a href="images/kdic_images/' + kdicImageNames[imageCounter + 2] + '" download> <p>Download</p> </a> </th> </tr>' );
     imageCounter = imageCounter + 3;
   }
 });
