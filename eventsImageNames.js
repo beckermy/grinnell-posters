95c8aad46a41c273f674859344907885b8791980
Addison Gould <gouldadd@grinnell.edu>
2017-09-20 22:16:24 -0500

    added more events posters

--- eventsImageNames.js
+++ eventsImageNames.js
@@ -1,12 +1,14 @@
 var evImageNames = [
   ["1988_April_gaypride.jpg", "Lesbian & Gay Pride Week", "Spring 1988"],
+  ["1986_spring_electronic.png", "Electronic Music Concert", "Spring 1986"],
+  ["1987_fall_deviate.jpeg", "Deviate Wildly Garder", "Fall 1987"],
+  ["1987_spring_disco.png", "Disco Gardner", "Spring 1987"],
+  ["1987_spring_norrisParty.png", "Norris Hall Wild Rumpus", "Spring 1987"],
   ["Alice1_2017.jpg", "Alice", "Spring 2017"],
   ["Alice2_2017.jpg", "Alice", "Spring 2017"],
   ["Alice3_2017.jpg", "Alice", "Spring 2017"],
   ["Alice4_2017.jpg", "Alice", "Spring 2017"],
   ["Alice5_2017.jpg", "Alice", "Spring 2017"],
-  ["Alice6_2017.jpg", "Alice", "Spring 2017"],
-  ["Alice6_2017.jpg", "Alice", "Spring 2017"],
   ["Alice6_2017.jpg", "Alice", "Spring 2017"]
 ];
 
